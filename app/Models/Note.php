a8b228802680fd1661bd622772451a10a36d242b
SergeyRushev <rushevs198417@gmail.com>
2023-10-09 11:18:53 +0300

    add: models

---
+++ app/Models/Note.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'content',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
